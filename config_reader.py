b4f626309fddc529cb1cc0f4379d30c1666a2db6
Prasanna Josium <prasanna.josium@dellteam.com>
2022-02-18 19:06:25 +0530

    extended configurations and updated cluster names

--- config_reader.py
+++ config_reader.py
@@ -1,7 +1,5 @@
-from importlib.abc import Loader
-from  yaml  import load, safe_load
-from os import path,getcwd
-from pathlib import Path
+from  yaml  import safe_load
+from os import path
 
 curdir= path.dirname(__file__)
 conf_file = path.join(curdir,'config.yml')
